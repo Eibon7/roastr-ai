726a6598ecc244e6326b8f7d96524921a77041b3
Eibon7 <Emiliopostigo@gmail.com>
2025-08-31 18:50:49 +0200

    fix: preserve 0 integrationsLimit

--- tests/helpers/testUtils.js
+++ tests/helpers/testUtils.js
@@ -208,7 +208,7 @@ const createMultiTenantTestScenario = (scenarioType = 'simple', options = {}) =>
   const finalEntitlements = {
     plan_name: entitlements.plan_name || effectivePlan,
     monthlyResponsesLimit: defaults.monthlyResponsesLimit,
-    integrationsLimit: entitlements.integrationsLimit || defaults.integrationsLimit || defaults.platforms,
+    integrationsLimit: entitlements.integrationsLimit ?? defaults.integrationsLimit ?? defaults.platforms,
     shieldEnabled: defaults.shieldEnabled,
     ...entitlements
   };
